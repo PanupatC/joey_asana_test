69d5e56d71e06ec94f2866e4b4fbfad58ed63735
PanupatC <panupatc@gmail.com>
2021-04-22 14:24:39 +0700

    update auth testing

--- asana_test/some_test.py
+++ asana_test/some_test.py
@@ -1,4 +1,5 @@
 import os
+import requests
 import asana
 from dotenv import load_dotenv
 
@@ -6,21 +7,49 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 
-# client_id = os.environ.get('client_id')
-# client_secret = os.environ.get('client_secret')
-# redirect_uri = os.environ.get('redirect_uri')
-# client = asana.Client.oauth(
-#   client_id='1200227892108962',
-#   client_secret='06373d89fe962454370fcf62de8711ad',
-#   redirect_uri='urn:ietf:wg:oauth:2.0:oob'
-# )
-#
-# url, state = client.session.authorization_url()
-#
-# print(f'Authorized = {client.session.authorized}')
+def secret_auth() -> asana.Client:
+    client_id = os.environ.get('client_id')
+    client_secret = os.environ.get('client_secret')
+    redirect_uri = os.environ.get('redirect_uri')
 
-token = os.environ.get('RealBkk_token')
-# token = os.environ.get('personal_Apps_token')
+    client = asana.Client.oauth(
+      client_id=client_id,
+      client_secret=client_secret,
+      redirect_uri=redirect_uri,
+      # response_type='code'
+    )
 
-client = asana.Client.access_token(token)
-print(f'Authorized = {client.session.authorized}')
+    url, state = client.session.authorization_url()
+
+    req = requests.get(url)
+    response = req.text
+    print(response)
+
+    return client
+
+
+def token_auth(token: str) -> asana.Client:
+    return asana.Client.access_token(token)
+
+if __name__ == '__main__':
+
+    secret = False
+
+    if secret:
+        client = secret_auth()
+    else:  # auth with token
+
+        token = os.environ.get('RealBkk_token')
+        # token = os.environ.get('personal_Apps_token')
+        client = token_auth(token)
+
+    print(f'Authorized = {client.session.authorized}')
+
+
+
+
+
+
+
+
+
