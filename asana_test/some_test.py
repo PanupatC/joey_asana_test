a02f96e103f7140203b64edd6538d4da382c41e9
PanupatC <panupatc@gmail.com>
2021-04-28 10:51:42 +0700

    minor test

--- asana_test/some_test.py
+++ asana_test/some_test.py
@@ -2,6 +2,7 @@ import os
 import requests
 import asana
 from dotenv import load_dotenv
+from typing import List
 
 
 load_dotenv('.env')
@@ -31,6 +32,17 @@ def secret_auth() -> asana.Client:
 def token_auth(token: str) -> asana.Client:
     return asana.Client.access_token(token)
 
+
+def get_all_projects(client: asana.Client) -> List[dict]:
+    workspace_gid = os.environ.get('workspace_gid')
+    return client.projects.find_by_workspace(workspace=workspace_gid)
+
+
+def get_project_by_name(client: asana.Client) -> List[dict]:
+    workspace_gid = os.environ.get('workspace_gid')
+    return []
+
+
 if __name__ == '__main__':
 
     secret = False
@@ -38,13 +50,23 @@ if __name__ == '__main__':
     if secret:
         client = secret_auth()
     else:  # auth with token
-
-        token = os.environ.get('RealBkk_token')
-        # token = os.environ.get('personal_Apps_token')
+        token = os.environ.get('personal_Apps_token')
         client = token_auth(token)
 
-    print(f'Authorized = {client.session.authorized}')
+    if not client.session.authorized:
+        print('Authorize failed')
+        quit()
 
+    # for _ in client.workspaces.find_all():
+    #     print(_)
+    #     workspace_gid = _['gid']
+    #     projects = client.projects.find_by_workspace(workspace=workspace_gid)
+    #     for p in projects:
+    #         print(p)
+
+    projects = get_all_projects(client)
+    for each in projects:
+        print(each)
 
 
 
